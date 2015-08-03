6bb72d66db9bc737541cddf53c908895da921f38
jdbrice <jdb12@rice.edu>
2015-08-03 15:58:17 -0500

    updating local_make.sh to be consistent with previous commit moving nominal->template

--- config/conf_maker/local_make.sh
+++ config/conf_maker/local_make.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
-./make.py ../auto -data_path ~/bnl/local/data/RcpAnalysis/ -output_path ~/bnl/local/work/rcpMaker/products/ -output_config_path ~/bnl/local/work/rcpMaker/config/auto/Params -common_path ../nominal/common/
+./make.py ../auto -data_path ~/bnl/local/data/RcpAnalysis/ -output_path ~/bnl/local/work/rcpMaker/products/ -output_config_path ~/bnl/local/work/rcpMaker/config/auto/Params -common_path ../template/common/
