d4ed2d91d5268a2f1f1267c94e938afdf3d33305
jdbrice <jdb12@rice.edu>
2015-08-04 13:07:12 -0500

    added the script for quick makes on macfrank

---
+++ config/conf_maker/mac_make.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./make.py /data/star/jdb12/RcpAnalysis/config/nominal -data_path /data/star/jdb12/RcpAnalysis/ -output_path /data/star/jdb12/RcpAnalysis/products/nominal/ -output_config_path /data/star/jdb12/RcpAnalysis/config/nominal/Params -common_path ../template/common/
