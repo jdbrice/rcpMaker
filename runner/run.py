ac4729a9ff1cdcedfb3155fab9b5c4b10ae12c20
jdbrice <jdb12@rice.edu>
2015-08-06 19:42:02 -0500

    added PostCorr and Present to runner/run.py

--- runner/run.py
+++ runner/run.py
@@ -11,12 +11,14 @@ import PidHisto.make_xml_configs as pidhc
 import TofEff.make_xml_configs as tofc
 import FeedDown.make_xml_configs as fdc
 import Fitter.make_xml_configs as fitc
+import PostCorr.make_xml_configs as postc
+import Present.make_xml_configs as presc
 
 parser = argparse.ArgumentParser( description="Runs the Rcp Analysis" );
 parser.add_argument("exe", help="path to executable")
 parser.add_argument("-config", help="Base path for config files", default="./" )
 
-parser.add_argument("task", help="Task to run. Can be one of : \n 'All', 'EnergyLoss', 'TpcEff', 'TofEff', 'FeedDown', 'PidHisto', 'Fit'" )
+parser.add_argument("task", help="Task to run. Can be one of : \n 'All', 'EnergyLoss', 'TpcEff', 'TofEff', 'FeedDown', 'PidHisto', 'Fit', 'PostCorr', 'Present'" )
 
 args = parser.parse_args();
 
@@ -27,7 +29,7 @@ print args
 plcs = ( "Pi", "K", "P" )
 charges = ( "p", "n" )
 track_types = ( "mc", "rc" )
-
+t_cmd = "{exe} {args} >& {log}"
 
 
 if "EnergyLoss" == args.task :
@@ -70,7 +72,7 @@ if "TpcEff" == args.task :
 				
 	config = os.path.join( args.config, "TpcEff", "fit.xml" )
 	logFile = config + ".log"
-	cmd = args.exe + " "  + config + " >& " + logFile
+	cmd = t_cmd.format( exe=args.exe, args=config, log=logFile )
 	print "Running : "
 	print cmd
 	os.system( cmd )
@@ -79,7 +81,7 @@ if "TpcEff" == args.task :
 if "FeedDown" == args.task :
 	config = os.path.join( args.config, "FeedDown", fdc.t_config_file.format( ext="xml" ) )
 	logFile = config + ".log"
-	cmd = args.exe + " "  + config + " >& " + logFile
+	cmd = t_cmd.format( exe=args.exe, args=config, log=logFile )
 	print "Running : "
 	print cmd
 	os.system( cmd )
@@ -89,13 +91,39 @@ if "PidHisto" == args.task :
 	for plc in plcs :
 		config = os.path.join( args.config, "PidHisto", pidhc.t_config_file.format( plc=plc, ext="xml" ) )
 		logFile = config + ".log"
-		os.system( args.exe + " "  + config + " >& " + logFile )
+		cmd = t_cmd.format( exe=args.exe, args=config, log=logFile )
+		print "Running : "
+		print cmd
+		os.system( cmd )
 
 if "Fit" == args.task :
 	for plc in plcs :
 		config = os.path.join( args.config, "Fitter", fitc.t_config_file.format( plc=plc, ext="xml" ) )
 		logFile = config + ".log"
-		os.system( args.exe + " "  + config + " >& " + logFile )
+		cmd = t_cmd.format( exe=args.exe, args=config, log=logFile )
+		print "Running : "
+		print cmd
+		os.system( cmd )
+
+if "PostCorr" == args.task :
+	for plc in plcs :
+		config = os.path.join( args.config, "PostCorr", postc.t_config_file( plc=plc, ext="xml" ) )
+		logFile = config + ".log"
+		cmd = t_cmd.format( exe=args.exe, args=config, log=logFile )
+		print "Running : "
+		print cmd
+		os.system( cmd )
+
+if "Present" == args.task :
+	for state in ( "Fit", "PostCorr" ) :
+		for plc in plcs :
+			config = os.path.join( args.config, "Present", postc.t_config_file( state=state, plc=plc, ext="xml" ) )
+			logFile = config + ".log"
+			cmd = t_cmd.format( exe=args.exe, args=config, log=logFile )
+			print "Running : "
+			print cmd
+			os.system( cmd )
+
 
 if "All" == args.task :
 	# not very clean but it will do
@@ -111,4 +139,9 @@ if "All" == args.task :
 	cmd = exeMe + args.exe + " PidHisto -config " + args.config
 	os.system( cmd )
 	cmd = exeMe + args.exe + " Fitter -config " + args.config
+	os.system( cmd )
+
+	cmd = exeMe + args.exe + " PostCorr -config " + args.config
+	os.system( cmd )
+	cmd = exeMe + args.exe + " Present -config " + args.config
 	os.system( cmd )
