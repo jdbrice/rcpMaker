f5286507b4b6cb7a2ea1ea940784b2fb3fe8aa85
jdbrice <jdb12@rice.edu>
2015-08-15 18:57:28 -0500

    Fixed bad path in mac_make.sh

--- runner/mac_make.sh
+++ runner/mac_make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./make.py /data/star/jdb12/RcpAnalysis/config/$1 -data_path /data/star/jdb12/RcpAnalysis/ -output_path /data/star/jdb12/RcpAnalysis/products/$1/ -output_config_path /data/star/jdb12/RcpAnalysis/config/$1/Params -common_path ../template/common/
+./make.py /data/star/jdb12/RcpAnalysis/config/$1 -data_path /data/star/jdb12/RcpAnalysis/ -output_path /data/star/jdb12/RcpAnalysis/products/$1/ -output_config_path /data/star/jdb12/RcpAnalysis/config/$1/Params -common_path ../config/template/common/
