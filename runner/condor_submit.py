4008326380018c3d41c8a4130e3577d3a4832842
jdbrice <jdb12@rice.edu>
2015-08-19 20:19:50 -0500

    added python script to make condor_submit files

---
+++ runner/condor_submit.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python2.7
+
+import argparse
+import glob
+import os
+
+parser = argparse.ArgumentParser( description="Creates the configs for Tasks" );
+parser.add_argument( "config_path", default="./config", help="creates the folder and stores all configs within")
+parser.add_argument( "submit_path", default="./config", help="creates the folder and stores all configs within")
+parser.add_argument( "list_path", default="./config", help="creates the folder and stores all configs within")
+parser.add_argument( "exe_path", default="./config", help="creates the folder and stores all configs within")
+
+
+
+t_submit="""
+#############################################
+# Create PidHisto files for PID Rcp Analysis
+#############################################
+
+Executable    = {exe}
+initialdir 		= {wd}
+"""
+
+t_arg = """
+Arguments = {cfg} {list} {prefix}
+Queue
+"""
+
+
+list_files = glob.glob( os.path.join( list_path, "list_*" )
+
+for f in list_files :
+	print f
+
+
