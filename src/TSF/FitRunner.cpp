9c303ad7941a7e3d2aa7ece14890d358a200d8c3
jdbrice <jdb12@rice.edu>
2016-04-20 15:08:01 -0500

    update plot titles for fit

--- src/TSF/FitRunner.cpp
+++ src/TSF/FitRunner.cpp
@@ -748,7 +748,7 @@ namespace TSF{
 		}
 
 		h->GetXaxis()->SetRangeUser( xMin, xMax );
-		h->GetYAxis()->SetTitleOffset( 1.4 );
+		h->GetYaxis()->SetTitleOffset( 1.4 );
 
 		h->SetTitle( ( setTitle( v ) + " : " +  dts((*binsPt)[ iPt ]) + " < p_{T} [GeV/c] < " + dts( (*binsPt)[ iPt + 1 ] ) ).c_str() );
 
