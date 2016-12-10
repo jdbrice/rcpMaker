620c5b8218444c801d6c5b670777cecb44894259
James Daniel Brandenburg <jdb12@rice.edu>
2016-12-10 01:56:08 -0600

    fix typo in MakeTpcEff.sh

--- bin/MakeTpcEff.sh
+++ bin/MakeTpcEff.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 echo "Pions"
-./rcp config/TpceEff_Make.xml --plc=Pi --charge=p --type=mc
-./rcp config/TpceEff_Make.xml --plc=Pi --charge=p --type=rc
+./rcp config/TpcEff_Make.xml --plc=Pi --charge=p --type=mc
+./rcp config/TpcEff_Make.xml --plc=Pi --charge=p --type=rc
 
-./rcp config/TpceEff_Make.xml --plc=Pi --charge=n --type=mc
-./rcp config/TpceEff_Make.xml --plc=Pi --charge=n --type=rc
+./rcp config/TpcEff_Make.xml --plc=Pi --charge=n --type=mc
+./rcp config/TpcEff_Make.xml --plc=Pi --charge=n --type=rc
 
 echo "Kaons"
-./rcp config/TpceEff_Make.xml --plc=K --charge=p --type=mc
-./rcp config/TpceEff_Make.xml --plc=K --charge=p --type=rc
+./rcp config/TpcEff_Make.xml --plc=K --charge=p --type=mc
+./rcp config/TpcEff_Make.xml --plc=K --charge=p --type=rc
 
-./rcp config/TpceEff_Make.xml --plc=K --charge=n --type=mc
-./rcp config/TpceEff_Make.xml --plc=K --charge=n --type=rc
+./rcp config/TpcEff_Make.xml --plc=K --charge=n --type=mc
+./rcp config/TpcEff_Make.xml --plc=K --charge=n --type=rc
 
 
 echo "Protons"
-./rcp config/TpceEff_Make.xml --plc=P --charge=p --type=mc
-./rcp config/TpceEff_Make.xml --plc=P --charge=p --type=rc
+./rcp config/TpcEff_Make.xml --plc=P --charge=p --type=mc
+./rcp config/TpcEff_Make.xml --plc=P --charge=p --type=rc
 
-./rcp config/TpceEff_Make.xml --plc=P --charge=n --type=mc
-./rcp config/TpceEff_Make.xml --plc=P --charge=n --type=rc
+./rcp config/TpcEff_Make.xml --plc=P --charge=n --type=mc
+./rcp config/TpcEff_Make.xml --plc=P --charge=n --type=rc
 
 
 echo "Fit"
